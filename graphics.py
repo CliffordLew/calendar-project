74f31f6ed32de3d172ffc232744054a196a88548
CliffordLew <clifski@gmail.com>
2024-04-04 17:08:46 -0500

    added unpolished textboxes for date and description

--- graphics.py
+++ graphics.py
@@ -1,4 +1,4 @@
-import main.py
+#import main.py
 from tkinter import *
 from tkinter import ttk
 
@@ -8,3 +8,20 @@ root.title("Calendar")
 
 ## content frame
 ## https://tkdocs.com/tutorial/firstexample.html
+
+## creating a content frame
+mainframe = ttk.Frame(root, padding = "50 5 0 100")
+mainframe.grid(column = 0, row = 0, sticky = (N, W, E, S))
+root.columnconfigure(0, weight = 1)
+root.rowconfigure(0, weight = 1)
+
+## creating date entry buttons
+date = StringVar()
+date_entry = ttk.Entry(mainframe, width = 100, textvariable = date)
+date_entry.grid(column = 2, row = 1, sticky = (W, E))
+
+description = StringVar()
+description_entry = ttk.Entry(mainframe, width = 100, textvariable = description)
+description_entry.grid(column = 2, row = 2, sticky = (W, E))
+
+root.mainloop()
